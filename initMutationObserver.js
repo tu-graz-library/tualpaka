8732278d2c43b991bdac590d2fa864e9c13e6294
Christoph Ladurner <christoph.ladurner@tugraz.at>
2020-01-14 10:19:22 +0100

    refactoring
    the MutationObserver call from Label is moved to tualpaka. the
    intention is to have a observer which listen to changes of the
    loadingBlockerStatusIdentifier. therefore it is now possible that
    multiple features add a callback to call when the observer fires.
    
    Label.js has now a function to add the button. This is added to the
    window.loadingBlockerEvents array. the elements from this array are
    called from callback given to the constructor of the MutationObserver.

---
+++ initMutationObserver.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2020 Christoph Ladurner, Technische Universität Graz, Bibliothek
+ *
+ * To print the labels in Alma the user has search with "Physische
+ * Exemplare". Then every physical items occur in the result list. The
+ * Print Label button is then on the right side on every result item
+ * hidden behind the three points.
+ *
+ * This extension only works with the German language package in Alma.
+ *
+ * init file to create the array of handle functions for the mutation
+ * observer which observes the #loadingBlockerStatusIdentifier element
+ *
+ **/
+
+window.loadingBlockerEvents = [];
